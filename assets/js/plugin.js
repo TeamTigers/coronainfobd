8e2d0a5d16c2aa0d76a9817280e2f9326d443559
zubayerhimel <zubayerhimel123@gmail.com>
2020-03-19 01:18:46 +0600

    added summary cards

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -11,35 +11,37 @@ $(document).ready(function() {
   $(".modal").modal();
   $(".dropdown-trigger").dropdown();
   $(".tabs").tabs();
-  $('.slider').slider();
-});
-//   // Fullscreen
-//   function toggleFullScreen() {
-//     if ((document.fullScreenElement && document.fullScreenElement !== null) ||
-//       (!document.mozFullScreen && !document.webkitIsFullScreen)) {
-//       if (document.documentElement.requestFullScreen) {
-//         document.documentElement.requestFullScreen();
-//       }
-//       else if (document.documentElement.mozRequestFullScreen) {
-//         document.documentElement.mozRequestFullScreen();
-//       }
-//       else if (document.documentElement.webkitRequestFullScreen) {
-//         document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-//       }
-//     }
-//     else {
-//       if (document.cancelFullScreen) {
-//         document.cancelFullScreen();
-//       }
-//       else if (document.mozCancelFullScreen) {
-//         document.mozCancelFullScreen();
-//       }
-//       else if (document.webkitCancelFullScreen) {
-//         document.webkitCancelFullScreen();
-//       }
-//     }
-//   }
+  $(".slider").slider();
 
-//   $('.toggle-fullscreen').click(function() {
-//     toggleFullScreen();
-//   });
+  var scrollLink = $(".scroll");
+
+  // Smooth scrolling
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $("body,html").animate(
+      {
+        scrollTop: $(this.hash).offset().top
+      },
+      1000
+    );
+  });
+
+  // Active link switching
+  $(window).scroll(function() {
+    var scrollbarLocation = $(this).scrollTop();
+
+    scrollLink.each(function() {
+      var sectionOffset = $(this.hash).offset().top - 20;
+
+      if (sectionOffset <= scrollbarLocation) {
+        $(this)
+          .parent()
+          .addClass("active");
+        $(this)
+          .parent()
+          .siblings()
+          .removeClass("active");
+      }
+    });
+  });
+});
