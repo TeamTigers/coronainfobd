e7a0f05f591ed85a69354ca5cb8e2c43ed5363b0
zubayerhimel <zubayerhimel123@gmail.com>
2020-03-19 02:07:22 +0600

    added case study

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -12,36 +12,4 @@ $(document).ready(function() {
   $(".dropdown-trigger").dropdown();
   $(".tabs").tabs();
   $(".slider").slider();
-
-  var scrollLink = $(".scroll");
-
-  // Smooth scrolling
-  scrollLink.click(function(e) {
-    e.preventDefault();
-    $("body,html").animate(
-      {
-        scrollTop: $(this.hash).offset().top
-      },
-      1000
-    );
-  });
-
-  // Active link switching
-  $(window).scroll(function() {
-    var scrollbarLocation = $(this).scrollTop();
-
-    scrollLink.each(function() {
-      var sectionOffset = $(this.hash).offset().top - 20;
-
-      if (sectionOffset <= scrollbarLocation) {
-        $(this)
-          .parent()
-          .addClass("active");
-        $(this)
-          .parent()
-          .siblings()
-          .removeClass("active");
-      }
-    });
-  });
 });
