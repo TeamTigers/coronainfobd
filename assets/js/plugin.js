d08e4c758254fa717c0ea5b27ad0cdbc49c0bcc6
zubayerhimel <zhimel007@gmail.com>
2020-03-18 21:16:58 +0600

    initial commit

---
+++ assets/js/plugin.js
@@ -0,0 +1,45 @@
+/*================================================================================
+  Item Name: Materialize - Material Design Admin Template
+  Version: 2.1
+  Author: GeeksLabs
+  Author URL: http://www.themeforest.net/user/geekslabs
+================================================================================*/
+
+$(document).ready(function() {
+  $(".tooltipped").tooltip();
+  $(".sidenav").sidenav();
+  $("select").formSelect();
+  $(".modal").modal();
+  $(".dropdown-trigger").dropdown();
+  $(".tabs").tabs();
+});
+//   // Fullscreen
+//   function toggleFullScreen() {
+//     if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+//       (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+//       if (document.documentElement.requestFullScreen) {
+//         document.documentElement.requestFullScreen();
+//       }
+//       else if (document.documentElement.mozRequestFullScreen) {
+//         document.documentElement.mozRequestFullScreen();
+//       }
+//       else if (document.documentElement.webkitRequestFullScreen) {
+//         document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+//       }
+//     }
+//     else {
+//       if (document.cancelFullScreen) {
+//         document.cancelFullScreen();
+//       }
+//       else if (document.mozCancelFullScreen) {
+//         document.mozCancelFullScreen();
+//       }
+//       else if (document.webkitCancelFullScreen) {
+//         document.webkitCancelFullScreen();
+//       }
+//     }
+//   }
+
+//   $('.toggle-fullscreen').click(function() {
+//     toggleFullScreen();
+//   });
