bab12beb19844d40f3af7e60415e9e09fb69782a
zubayerhimel <zhimel007@gmail.com>
2020-03-19 18:58:11 +0600

    removed collapsible and added card for the table

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -8,9 +8,6 @@
 $(document).ready(function() {
   $(".tooltipped").tooltip();
   $(".sidenav").sidenav();
-  $(".modal").modal();
-  $(".dropdown-trigger").dropdown();
-  $(".tabs").tabs();
   $(".slider").slider();
   $(".collapsible").collapsible();
 
