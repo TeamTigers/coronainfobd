b8f7fa0fe774d7c9412d600416cf26451e5f865c
jinnatul <morolswediu@gmail.com>
2020-04-22 01:19:57 +0600

    make js array to table

--- assets/js/dhakaCity.js
+++ assets/js/dhakaCity.js
@@ -9,3 +9,13 @@ $(function() {
             showToast("Something went wrong!");
         });
 });
+ 
+function makeTable(dhakacityArray) {
+    let tableStr = "<table><thead><tr><th>Location</th><th>Total</th></tr></thead><tbody id='locationTotal'>";
+    for (let index = 0; index < dhakacityArray.length; index++) {
+        tableStr += "<tr><td>"+dhakacityArray[index][0]+"</td><td>"
+                        +dhakacityArray[index][1]+"</td></tr>"
+    }
+    tableStr += "</tbody></table>";
+    $('#dhakaCityInfo').html(tableStr)
+} 
