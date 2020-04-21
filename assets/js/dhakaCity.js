bfdf79384f99cafa34e3414712d205025cf67576
jinnatul <morolswediu@gmail.com>
2020-04-22 01:17:18 +0600

    convert json to js array

--- assets/js/dhakaCity.js
+++ assets/js/dhakaCity.js
@@ -2,8 +2,8 @@ $(function() {
     let dhakaCityURL = "https://teamtigers.github.io/covid19-dataset-bd/dhakacity/dhakacity.json";
     $.get(dhakaCityURL, function(){}) 
         .done(function(res) {
-            //let dhakacityArray = Object.entries(res);
-            //makeTable(dhakacityArray);
+            let dhakacityArray = Object.entries(res);
+            makeTable(dhakacityArray);
         })
         .fail(function () {
             showToast("Something went wrong!");
