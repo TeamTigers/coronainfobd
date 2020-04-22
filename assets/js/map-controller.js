b097f3da201a8fd6f6b2a6afb75252ea8c4646c4
shunjid <shunjid1862@gmail.com>
2020-04-23 01:27:15 +0600

    simplified logic

--- assets/js/map-controller.js
+++ assets/js/map-controller.js
@@ -1,27 +1,29 @@
-$(function() { 
+$(function () {
     let districtUrl = "https://corona-bd.herokuapp.com/district";
     let districtData = [];
 
-    $.get(districtUrl, function(){}) 
-    .done(function(response) {
-        districtData = response.data;
-    });
-    
+    $.get(districtUrl, function () {})
+        .done(function (response) {
+            districtData = response.data;
+        });
+
     $('#mapColumn a').on('click mouseover', function () {
         let districtName = $(this).data("value");
         let districtId = $(this).data("id");
-        let selectedDistrict = districtData.find(o => o.id === districtId);
+        let selectedDistrict = districtData.find(o => o.id === districtId) || districtData.find(o => o.name === districtName);
 
-        if(selectedDistrict == null) {
-            selectedDistrict = districtData.find(o => o.name === districtName);
-        }
-
-        if(selectedDistrict == null) {
-            $('#selectedDistrict').text(districtName);
-            $('#selectedDistrictCount').text(0);
-        } else {
-            $('#selectedDistrict').text(selectedDistrict.name);
-            $('#selectedDistrictCount').text(selectedDistrict.count);
-        }
+        selectedDistrict == null ? 
+                            setValueWhenNotFound(districtName) :
+                            setSelectedValue(selectedDistrict.name, selectedDistrict.count);
     });
 });
+
+function setValueWhenNotFound(selectedDistrictName) {
+    $('#selectedDistrict').text(selectedDistrictName);
+    $('#selectedDistrictCount').text(0);
+}
+
+function setSelectedValue(districtName, districtAffected) {
+    $('#selectedDistrict').text(districtName);
+    $('#selectedDistrictCount').text(districtAffected);
+}
