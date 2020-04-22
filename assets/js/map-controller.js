39b4e5e53b842f2b4be792ca37cc4277a57a84ee
shunjid <shunjid1862@gmail.com>
2020-04-23 01:42:18 +0600

    added card for percentage

--- assets/js/map-controller.js
+++ assets/js/map-controller.js
@@ -1,29 +1,37 @@
 $(function () {
     let districtUrl = "https://corona-bd.herokuapp.com/district";
     let districtData = [];
+    let totalInfected = 0;
 
     $.get(districtUrl, function () {})
         .done(function (response) {
             districtData = response.data;
+            totalInfected = response.total_infected;
         });
 
     $('#mapColumn a').on('click mouseover', function () {
-        let districtName = $(this).data("value");
         let districtId = $(this).data("id");
+        let districtName = $(this).data("value");
         let selectedDistrict = districtData.find(o => o.id === districtId) || districtData.find(o => o.name === districtName);
 
-        selectedDistrict == null ? 
-                            setValueWhenNotFound(districtName) :
-                            setSelectedValue(selectedDistrict.name, selectedDistrict.count);
+        selectedDistrict == null ?
+            setValueWhenNotFound(districtName) :
+            setSelectedValue(
+                selectedDistrict.name,
+                selectedDistrict.count,
+                (selectedDistrict.count/totalInfected)*100
+            );
     });
 });
 
 function setValueWhenNotFound(selectedDistrictName) {
     $('#selectedDistrict').text(selectedDistrictName);
     $('#selectedDistrictCount').text(0);
+    $('#selectedDistrictPercentage').text('0 %');
 }
 
-function setSelectedValue(districtName, districtAffected) {
+function setSelectedValue(districtName, districtAffected, percentage) {
     $('#selectedDistrict').text(districtName);
     $('#selectedDistrictCount').text(districtAffected);
+    $('#selectedDistrictPercentage').text(percentage.toFixed(2).toString().concat(' %'));
 }
